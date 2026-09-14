4ca364000df3d16144655a9a3a115e6ed77c10a8
h-pl <51890982+h-pl@users.noreply.github.com>
2026-09-14 21:33:54 +0800

    fix: use default Next output directory on Vercel

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   devIndicators: false,
-  distDir: '.next-platform',
+  distDir: process.env.VERCEL ? '.next' : '.next-platform',
   webpack(config, { webpack }) {
     config.plugins.push(new webpack.DefinePlugin({ __VUE_OPTIONS_API__: true, __VUE_PROD_DEVTOOLS__: false, __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: false }));
     return config;
